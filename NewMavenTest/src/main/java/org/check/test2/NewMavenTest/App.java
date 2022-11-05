f6da254f6701ac54e1b773bf0393e099f09ded10
atulmandhotra <atulmandhotra9@gmail.com>
2022-11-06 02:40:09 +0530

    hi master

--- NewMavenTest/src/main/java/org/check/test2/NewMavenTest/App.java
+++ NewMavenTest/src/main/java/org/check/test2/NewMavenTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Atul goku!" );
+        System.out.println( "Hello World Atul goku! hi 123" );
     }
 }
